80867e12cddf000a66b9205c832dc4d8c211582a
Chengmu Joe Yan <cyan@lawndale.org>
2017-11-17 15:10:14 -0600

    Add solution to problem #2 that doesn't require even-checking

--- 2.py
+++ 2.py
@@ -10,4 +10,18 @@ def even_fib(limit):
         a, b = b, a+b
 print("Sol 1 - even fib generator")
 print(sum(even_fib(4 * 10**6)))
-print(timeit.timeit("sum(even_fib(4 * 10**6))", 'from __main__ import even_fib', number=1000))
+print(timeit.timeit("sum(even_fib(4 * 10**20))", 'from __main__ import even_fib', number=1000))
+
+# Sol 2 - even fib generator, no even check
+def even_fib(limit):
+    a = 0
+    b = 1
+    c = 2
+    while (c < limit):
+        yield c
+        a = b + c
+        b = c + a
+        c = a + b
+print("Sol 2")
+print(sum(even_fib(4 * 10**6)))
+print(timeit.timeit("sum(even_fib(4 * 10**20))", 'from __main__ import even_fib', number=1000))
