5ab8a177e102f5ecfdaf69cc7ddfd80562e2f2fd
Chengmu Joe Yan <cyan@lawndale.org>
2017-11-17 14:44:49 -0600

    Add timing to problem #2

--- 2.py
+++ 2.py
@@ -1,3 +1,6 @@
+import timeit
+
+# Sol 1 - even fib generator
 def even_fib(limit):
     a = 1
     b = 2
@@ -5,5 +8,6 @@ def even_fib(limit):
         if not a % 2:
             yield a
         a, b = b, a+b
-
+print("Sol 1 - even fib generator")
 print(sum(even_fib(4 * 10**6)))
+print(timeit.timeit("sum(even_fib(4 * 10**6))", 'from __main__ import even_fib', number=1000))
