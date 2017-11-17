587a9217337e506e64ecef38faad2d8bd5bd7653
Chengmu Joe Yan <cyan@lawndale.org>
2017-11-17 17:26:53 -0600

    Work on problem #3

--- 3.py
+++ 3.py
@@ -4,15 +4,25 @@ def check_prime(n):
             return False
     return True
 
-# generate primes up to n using sieve
-def generate_primes(n):
+# generate composites up to n
+def generate_composites(n):
+    top = int(n ** 0.5)
+    comps = set()
+    for factor in range(2, top + 1):
+        for mult in range(1, int(n / factor)):
+            comps.add(factor * mult)
+    return comps
 
 num = 600851475143
-for div in range(num // 2 + 1, 1, -1):
-    if not div % 10**7:
-        print(div)
-    if not num % div:
-        print(div)
+comps = generate_composites(num // 2)
+sorted_comps = sorted(list(comps))
+
+index = len(sorted_comps) - 1
+for div in range(num // 2, 1, -1):
+    if div == sorted_comps[index]:
+        # don't test this, because it's a prime
+        index -= 1
+    else:
 
 
 # for div in range(num // 2 + 1, 1, -1):
